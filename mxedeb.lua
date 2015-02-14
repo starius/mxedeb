52e425190b7f3b265027fa1d8eeeec258e671bc8
Boris Nagaev <bnagaev@gmail.com>
2015-02-14 15:23:36 +0100

    speed up getting dependencies
    MXE master is too slow to run make multiple times.
    All packages and all dependencies are printed by single
    make command.

--- mxedeb.lua
+++ mxedeb.lua
@@ -29,40 +29,32 @@ local function trim(str)
     return text
 end
 
-local function allPackages()
-    local pkgs = {}
-    local ls = io.popen('ls src/*.mk', 'r')
-    for line in ls:lines() do
-        local pkg = line:match("src/(.+).mk")
-        table.insert(pkgs, pkg)
-    end
-    ls:close()
-    return pkgs
-end
-
--- return table which maps name of package to list of deps
-local function allDeps(pkgs)
+-- return list of table and map from package to list of deps
+local function pkgsAndDeps()
     -- create file deps.mk showing deps
     -- (make show-upstream-deps-% does not present in
     -- stable MXE)
     local deps_mk_content = [[
 include Makefile
-print-%-deps:
-	@echo $($*_DEPS)]]
+print-deps:
+	@$(foreach pkg,$(PKGS),echo $(pkg) $($(pkg)_DEPS);)]]
     local deps_mk_file = io.open('deps.mk', 'w')
     deps_mk_file:write(deps_mk_content)
     deps_mk_file:close()
     local pkg2deps = {}
-    for _, pkg in ipairs(pkgs) do
-        local cmd = 'make -f deps.mk print-%s-deps'
-        local make = io.popen(cmd:format(pkg))
-        local deps_str = make:read('*a')
-        make:close()
-        local deps = split(trim(deps_str))
+    local pkgs = {}
+    local cmd = 'make -f deps.mk print-deps'
+    local make = io.popen(cmd)
+    for line in make:lines() do
+        local deps = split(trim(line))
+        -- first value is name of package which depends on
+        local pkg = table.remove(deps, 1)
         pkg2deps[pkg] = deps
+        table.insert(pkgs, pkg)
     end
+    make:close()
     os.remove('deps.mk')
-    return pkg2deps
+    return pkgs, pkg2deps
 end
 
 -- return list of direct and indirect dependencies
@@ -94,8 +86,7 @@ local function dependencyGraph(pkgs, pkg2deps)
     return graph
 end
 
-local pkgs = allPackages()
-local pkg2deps = allDeps(pkgs)
+local pkgs, pkg2deps = pkgsAndDeps()
 local graph = dependencyGraph(pkgs, pkg2deps)
 
 for pkg1, deps in pairs(graph) do
